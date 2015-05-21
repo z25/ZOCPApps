e506ef83f391b210e80a89d33f91f2118036edbc
Arnaud Loonstra <arnaud@sphaero.org>
2015-05-21 16:45:02 +0000

    fix GLES compatibility

--- zvideo/shader.glsl
+++ zvideo/shader.glsl
@@ -7,6 +7,5 @@ uniform float alpha;
 uniform vec3 color;
 
 void main() {
-    //gl_FragColor = vec4(1.0,0,0,0.1);
-    gl_FragColor = (texture2D( tex, v_texcoord ) * alpha) + vec4(color*(1-alpha),1);
+    gl_FragColor = (texture2D( tex, v_texcoord ) * alpha) + vec4(color*(1.0-alpha),1.0);
 }
